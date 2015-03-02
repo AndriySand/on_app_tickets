0b565bf3755207ec15fa471ec3e30e79e2a62dce
Андрий <andriy@cyberdainecorp.(none)>
2015-03-02 21:12:42 +0200

    Ticket model.

---
+++ app/models/ticket.rb
@@ -0,0 +1,20 @@
+class Ticket < ActiveRecord::Base
+  validates :user_name, :user_email, :subject, :department, :presence => true
+  after_create :ensure_reference_number
+
+  def ensure_reference_number
+    arr = (0...3).map{[get_string_part, get_hex_number]}.flatten
+  	arr.pop
+    self.reference_number = arr.join('-')
+    self.save
+  end
+
+  def get_string_part
+  	(0...3).map{ ('A'..'Z').to_a[rand(26)] }.join
+  end
+
+  def get_hex_number
+  	SecureRandom.hex(1).upcase
+  end
+
+end
