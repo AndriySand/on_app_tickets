c70159198e8f2ec338d979210d1c8fbe0b55428c
Андрий <andriy@cyberdainecorp.(none)>
2015-03-02 15:54:12 +0200

    Initial commit.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+OnAppTickets::Application.config.secret_key_base = 'c4fd2337a10eccb9b940f63e142ea4899c5a9de51605e05392076e8187b880218edb5cc16b834920c96952450d55ce57d736a6f4290250fbfc4c70d809624bc2'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+OnAppTickets::Application.config.session_store :cookie_store, key: '_on_app_tickets_session'
