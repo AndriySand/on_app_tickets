7894d097588875eb32d7860f300721de17e4cc99
Андрий <andriy@cyberdainecorp.(none)>
2015-03-07 21:26:49 +0200

    Model User 'has_many' replies.

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,5 @@ class User < ActiveRecord::Base
   validates :name, :uniqueness => true, :presence => true
   validates :password, :presence => true
   has_many :tickets, :foreign_key => "manager_id"
+  has_many :replies, :foreign_key => "author_id"
 end
