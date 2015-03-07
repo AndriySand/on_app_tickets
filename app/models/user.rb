86450538e2a5a7d4984f5812b554f469dd529811
Андрий <andriy@cyberdainecorp.(none)>
2015-03-07 17:18:42 +0200

    Model User 'has_many' tickets.

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,5 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable, :rememberable, :trackable
   validates :name, :uniqueness => true, :presence => true
   validates :password, :presence => true
+  has_many :tickets, :foreign_key => "manager_id"
 end
