c2724e8c4f3b28951612cc513d3ca079805ebab1
Андрий <andriy@cyberdainecorp.(none)>
2015-03-09 08:19:42 +0200

    JavaScript 'take_responsibility' changes status of ticket also.

--- app/assets/javascripts/take_responsibility.js
+++ app/assets/javascripts/take_responsibility.js
@@ -1,14 +1,23 @@
 $(function (){
-  $('#manager_id').bind('change', function(){
+  $('#manager_id, #select_status').bind('change', function(){
   	var user_id = $(this).attr('user_id');
     var ticket_id = $(this).attr('ticket_id');
-    $.ajax({
-      url: "/tickets/".concat(ticket_id, "/take_responsibility"),
-      type: 'POST',
-      data: {user_id: user_id, ticket_id: ticket_id }
-    });
-    $(document).ajaxSuccess(function(event, xhr, settings) {
-      alert(xhr.responseText);
+    var status = $("#select_status option:selected").text();
+      if(user_id == undefined){
+        var params = {status: status };
+        var url = "/tickets/".concat(ticket_id, "/change_status");
+      }
+      else{
+        var params = {user_id: user_id, ticket_id: ticket_id };
+        var url = "/tickets/".concat(ticket_id, "/take_responsibility");
+      }
+      $.ajax({
+        url: url,
+        type: 'POST',
+        data: params
+      });
+      $(document).ajaxSuccess(function(event, xhr, settings) {
+        alert(xhr.responseText);
     });
   });
 });
