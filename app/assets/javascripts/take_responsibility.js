b3f4205d547f5a26b72c80cfbf21978e8a8a1c0e
Андрий <andriy@cyberdainecorp.(none)>
2015-03-09 21:18:30 +0200

    Added JavaScript which allows to add new values for ticket's statuses.

--- app/assets/javascripts/take_responsibility.js
+++ app/assets/javascripts/take_responsibility.js
@@ -11,13 +11,35 @@ $(function (){
         var params = {user_id: user_id, ticket_id: ticket_id };
         var url = "/tickets/".concat(ticket_id, "/take_responsibility");
       }
-      $.ajax({
-        url: url,
-        type: 'POST',
-        data: params
-      });
+      send_ajax(url, 'POST', params)
       $(document).ajaxSuccess(function(event, xhr, settings) {
         alert(xhr.responseText);
     });
   });
 });
+
+$(function (){
+  $('#new_status').bind('blur', function(){
+    var status = $(this).prop('value');
+    var ticket_id = $('#select_status').attr('ticket_id');
+    if(status.length < 5){
+      alert("The length of new status should be longer then 4 and includes only a - z symbols")
+    }
+    else{
+      var params = {status: status };
+      var url = "/tickets/".concat(ticket_id, "/change_status");
+      send_ajax(url, 'POST', params)
+      $(document).ajaxSuccess(function(event, xhr, settings) {
+        alert(xhr.responseText);
+      });
+    }
+  });
+});
+
+function send_ajax(url, request_type, params){
+  $.ajax({
+    url: url,
+    type: request_type,
+    data: params
+  });
+};
