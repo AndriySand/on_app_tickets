447a606889a0654531292b04ed403ea2b9f415e8
Андрий <andriy@cyberdainecorp.(none)>
2015-03-09 08:43:44 +0200

    Seeds file to fulfill database.

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,14 @@
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+
+namespace :db do
+departments = ['support', 'production', 'australia', 'canada']
+statuses = ['Waiting for Staff Response', 'Waiting for Customer', 'On Hold', 'Cancelled', 'Completed']
+  Rake::Task['db:reset'].invoke
+
+  100.times do |a|
+  	Ticket.create(:user_email => "australia-#{a + 1}@mail.ru", :user_name => Faker::Name.name,
+  		:department => departments[rand(0...4)], :subject => Faker::Lorem.sentence,
+  		:status => statuses[rand(0...5)], :body => Faker::Lorem.sentence(5))
+  end
+end
