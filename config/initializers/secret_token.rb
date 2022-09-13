3a43a064b5ac752bbef989dfd0a3925d9b42c039
Andriy Samoxin <australi@inbox.ru>
2022-09-13 23:16:15 +0300

    Update secret_token.rb

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -9,4 +9,3 @@
 
 # Make sure your secret_key_base is kept private
 # if you're sharing your code publicly.
-OnAppTickets::Application.config.secret_key_base = 'c4fd2337a10eccb9b940f63e142ea4899c5a9de51605e05392076e8187b880218edb5cc16b834920c96952450d55ce57d736a6f4290250fbfc4c70d809624bc2'
